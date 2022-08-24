d21d337ee22bb33edc8e0f2c91bb9916d46054f9
Peter Jones <peterjjones98@gmail.com>
2022-08-24 15:08:26 -0700

    Restructured Files.

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,36 +1,37 @@
 defmodule Frame do
-  defstruct points: 0, state: :start, roll: 0
+  @moduledoc"""
+    State machine for a frame in a bowling game.
+  """
 
+  defstruct points: 0, state: :start, roll: 0
+  defguard is_spare(points, roll) when points + roll == 10
+
+  # :start states
   def roll_event(%Frame{state: :start, roll: roll}) when roll < 10 do
-    %Frame{state: :done, points: roll}
+    %Frame{state: :open, points: roll}
   end
 
   def roll_event(%Frame{state: :start, roll: 10}) do
-    %Frame{state: :done, points: 10}
+    %Frame{state: :strike, points: 10}
+  end
+
+  # :open states
+  def roll_event(%Frame{state: :open, points: points, roll: roll }) when is_spare(points, roll) do
+    %Frame{state: :spare, points: points + roll}
+  end
+
+  def roll_event(%Frame{state: :open, points: points, roll: roll}) do
+    %Frame{state: :done, points: points + roll}
+  end
+
+  # :spare states
+  def roll_event(%Frame{state: :spare, points: points, roll: roll}) do
+    %Frame{state: :done, points: points + roll}
+  end
+
+  # :stike states
+  def roll_event(%Frame{state: :strike, points: points, roll: roll}) when roll == 10 do
+    %Frame{state: :strike, points: points + roll} #this aint gonna work bruh
   end
 
 end
-
-defmodule Game do
-  defguard not_done(frame) when frame.state != :done
-
-  def score_frame(frame, _) when frame.state == :done, do: frame.points
-  def score_frame(frame, []), do: frame.points
-  def score_frame(frame, next_frames) when not_done(frame) do
-    generate_frame(frame, next_frames)
-  end
-
-
-  def generate_frame(frame, [head | tail]) when not_done(frame) do
-    frame = %{frame | roll: head}
-    new_frame = Frame.roll_event(frame)
-    score_frame(new_frame, tail)
-  end
-
-  def do_stuff(frames) do
-    generate_frame(%Frame{}, frames)
-  end
-end
-
-new_game = Game
-IO.inspect(new_game.do_stuff([1,2,3,4,5])) #should == 10
---
+++ lib/game.ex
@@ -0,0 +1,7 @@
+defmodule Game do
+  import Frame
+  @moduledoc"""
+    Acts as an orchestrator to the Frame state machines.
+  """
+
+end
